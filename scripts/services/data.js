991187a086e0865a9432854ddba874795cac93a2
derekmhawkins <derekmhawkins@gmail.com>
2017-03-01 06:23:21 -0600

    First commi

---
+++ scripts/services/data.js
@@ -0,0 +1,15 @@
+'use strict';
+angular.module('todoListApp')
+    .service('dataService', function ($http) {
+        this.getTodos = function (callback) {
+            $http.get('mock/todos.json').then(callback);
+        };
+    
+        this.deleteTodo = function (todo) {
+            console.log("The '" + todo.name + "' todo has been deleted.");
+        };
+    
+        this.saveTodos = function (todo) {
+            console.log("The '" + todo.name + "' todo was saved.");
+        };
+    });
